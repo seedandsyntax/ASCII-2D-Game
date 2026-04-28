7539d38ec8cf7e1c4d613351d604490e5d9fd8aa
Creitin Gameplays <clton999@outlook.com>
2026-04-28 10:24:05 -0300

    initial experimentation

---
+++ src/engine/terminal-manager.cpp
@@ -0,0 +1,27 @@
+#include <curses.h>
+#include <print>
+
+int main(){
+    initscr();
+    cbreak();
+    keypad(stdscr, 1);
+
+    int input;
+    size_t times {0};
+    while ((input = getch()) != 'q'){ // press "q" to exist
+        if (input == KEY_UP) {
+            times++;
+            printw("arrow up detected: %d\n", times);
+            refresh();
+            if (times % 10 == 0) clear();
+        }
+
+        if (input == 'c'){
+            clear(); // clear terminal
+        }
+    }
+    
+    std::println("Goodbye!");
+    endwin();
+    return 0;
+}
